e017b3c6245d5a5badfc01a5eff4fab4881846ca
Per <atlasfreak@gmx.de>
2024-06-03 14:55:50 +0200

    remove some functions to reduce classes

--- src/main/java/h07/Main.java
+++ src/main/java/h07/Main.java
@@ -1,8 +1,14 @@
 package h07;
 
-import h07.Peano.*;
 import org.tudalgo.algoutils.student.annotation.DoNotTouch;
 import org.tudalgo.algoutils.student.annotation.StudentImplementationRequired;
+
+import h07.Peano.NaturalNumber;
+import h07.Peano.PeanoAddExpression;
+import h07.Peano.PeanoMultiplyExpression;
+import h07.Peano.PeanoNumberExpression;
+import h07.Peano.PeanoNumberExpressionFactory;
+import h07.Peano.Zero;
 
 /**
  * Main entry point in executing the program.
@@ -16,12 +22,6 @@ public class Main {
     public static void main(String[] args) {
         testHeader("Multiplication Table");
         numberExpressionMultiplicationTableTests();
-
-        testHeader("Modulo Table");
-        numberExpressionModuloTableTests();
-
-        testHeader("Complex Expressions");
-        numberExpressionComplexTests();
 
         testHeader("Peano Number Expressions");
         peanoNumberExpressionTests();
@@ -46,50 +46,27 @@ public class Main {
     }
 
     @StudentImplementationRequired
-    private static void numberExpressionModuloTableTests() {
-        NumberExpression[] multiplicationTable = NumberExpressionFactory.moduloTable(1, 10);
-        for (NumberExpression arithmeticExpression : multiplicationTable) {
-            System.out.println(arithmeticExpression.evaluate());
-        }
-    }
-
-    @StudentImplementationRequired
-    private static void numberExpressionComplexTests() {
-        NumberExpression[] a = NumberExpressionFactory.multiplicationTable(2, 4);
-        NumberExpression[] b = NumberExpressionFactory.multiplicationTable(10, 13);
-        NumberExpression[] c = NumberExpressionFactory.multiplicationTable(3, 6);
-        NumberExpression[] d = NumberExpressionFactory.moduloTable(8, 11);
-        assert a.length == b.length && b.length == c.length && c.length == d.length;
-
-        NumberExpression[] complexExpressions = NumberExpressionFactory.complexExpression(a, b, c, d);
-        for (NumberExpression arithmeticExpression : complexExpressions) {
-            System.out.println(arithmeticExpression.evaluate());
-        }
-    }
-
-    @StudentImplementationRequired
     private static void peanoNumberExpressionTests() {
         PeanoNumberExpression three = new ConvertNumberToPeanoExpressionImpl().convert(() -> 3);
         PeanoNumberExpression seven = new ConvertNumberToPeanoExpressionImpl().convert(() -> 7);
 
         PeanoNumberExpression sum = new PeanoAddExpression().evaluate(three, seven);
         PeanoNumberExpression product = new PeanoMultiplyExpression().evaluate(three, seven);
-        PeanoNumberExpression subtract = new PeanoSubtractionExpression().evaluate(seven, three);
 
         System.out.println(sum.evaluate());
         System.out.println(new ConvertPeanoToNumberExpressionImpl().convert(sum).evaluate());
         System.out.println(product.evaluate());
         System.out.println(new ConvertPeanoToNumberExpressionImpl().convert(product).evaluate());
-        System.out.println(subtract.evaluate());
-        System.out.println(new ConvertPeanoToNumberExpressionImpl().convert(subtract).evaluate());
     }
 
     @StudentImplementationRequired
     private static void filterFoldMapTests() {
         NumberExpression[] numbers = NumberExpressionFactory.multiplicationTable(1, 3);
         NumberExpression[] filteredNumbers = NumberExpressionFactory.filter(numbers, n -> n % 2 == 0);
-        PeanoNumberExpression[] filteredPeanoNumbers = PeanoNumberExpressionFactory.fromNumberExpressions(filteredNumbers);
-        PeanoNumberExpression foldedPeanoNumbers = PeanoNumberExpressionFactory.fold(filteredPeanoNumbers, Zero::new, new PeanoAddExpression());
+        PeanoNumberExpression[] filteredPeanoNumbers = PeanoNumberExpressionFactory
+                .fromNumberExpressions(filteredNumbers);
+        PeanoNumberExpression foldedPeanoNumbers = PeanoNumberExpressionFactory.fold(filteredPeanoNumbers, Zero::new,
+                new PeanoAddExpression());
         NaturalNumber foldedPeanoNumber = foldedPeanoNumbers.evaluate();
         System.out.println(foldedPeanoNumber);
         int foldedNumber = new ConvertPeanoToNumberExpressionImpl().convert(foldedPeanoNumbers).evaluate();
--- src/main/java/h07/NumberExpressionFactory.java
+++ src/main/java/h07/NumberExpressionFactory.java
@@ -1,8 +1,8 @@
 package h07;
 
-import org.tudalgo.algoutils.student.annotation.StudentImplementationRequired;
-
 import java.util.function.IntPredicate;
+
+import org.tudalgo.algoutils.student.annotation.StudentImplementationRequired;
 
 /**
  * A factory class for creating number expressions.
@@ -14,7 +14,8 @@ public class NumberExpressionFactory {
      *
      * @param lowerBound the lower bound of the multiplication table, inclusive
      * @param upperBound the upper bound of the multiplication table, inclusive
-     * @return An array of number expressions representing the result of the multiplication table of the numbers from lowerBound to upperBound.
+     * @return An array of number expressions representing the result of the
+     *         multiplication table of the numbers from lowerBound to upperBound.
      */
     public static NumberExpression[] multiplicationTable(int lowerBound, int upperBound) {
         int numberOfNumbers = upperBound - lowerBound + 1;
@@ -22,7 +23,8 @@ public class NumberExpressionFactory {
         NumberExpression[] numbers = new NumberExpression[numberOfNumbers * numberOfNumbers];
 
         for (int i = lowerBound; i <= upperBound; i++) {
-            // Copy to local variable to make it effectively final, so it can be used in lambda
+            // Copy to local variable to make it effectively final, so it can be used in
+            // lambda
             int finalI = i;
             baseNumbers[i - lowerBound] = () -> finalI;
         }
@@ -39,71 +41,12 @@ public class NumberExpressionFactory {
     }
 
     /**
-     * Calulates the modulo of all possible pairs of numbers in the given range.
-     *
-     * @param lowerBound the lower bound of the modulo table, inclusive
-     * @param upperBound the upper bound of the modulo table, inclusive
-     * @return An array of number expressions representing the result of the modulo table of the numbers from lowerBound to upperBound.
-     */
-    @StudentImplementationRequired
-    public static NumberExpression[] moduloTable(int lowerBound, int upperBound) {
-        int numberOfNumbers = upperBound - lowerBound + 1;
-        NumberExpression[] baseNumbers = new NumberExpression[numberOfNumbers];
-        NumberExpression[] numbers = new NumberExpression[numberOfNumbers * numberOfNumbers];
-
-        for (int i = 0; i < numberOfNumbers; i++) {
-            // Copy to local variable to make it effectively final, so it can be used in lambda
-            int finalI = i;
-            baseNumbers[i] = () -> finalI + lowerBound;
-        }
-
-        ArithmeticExpression modulo = (num1, num2) -> () -> num1.evaluate() % num2.evaluate();
-
-        for (int i = 0; i < numberOfNumbers; i++) {
-            for (int j = 0; j < numberOfNumbers; j++) {
-                numbers[i * numberOfNumbers + j] = modulo.evaluate(baseNumbers[i], baseNumbers[j]);
-            }
-        }
-
-        return numbers;
-    }
-
-    /**
-     * Computes (a * (b - c) + d) % a for each element in the arrays.
-     *
-     * It is guaranteed that the arrays have the same length.
-     *
-     * @param a an array of number expressions
-     * @param b an array of number expressions
-     * @param c an array of number expressions
-     * @param d an array of number expressions
-     * @return An array of number expressions representing the result of the complex expression.
-     */
-    @StudentImplementationRequired
-    public static NumberExpression[] complexExpression(NumberExpression[] a, NumberExpression[] b, NumberExpression[] c, NumberExpression[] d) {
-        NumberExpression[] result = new NumberExpression[a.length];
-
-        ArithmeticExpression multiplication = (num1, num2) -> () -> num1.evaluate() * num2.evaluate();
-        ArithmeticExpression addition = (num1, num2) -> () -> num1.evaluate() + num2.evaluate();
-        ArithmeticExpression subtraction = (num1, num2) -> () -> num1.evaluate() - num2.evaluate();
-        ArithmeticExpression modulo = (num1, num2) -> () -> num1.evaluate() % num2.evaluate();
-
-        for (int i = 0; i < a.length; i++) {
-            NumberExpression bMinusC = subtraction.evaluate(b[i], c[i]);
-            NumberExpression aTimesBMinusC = multiplication.evaluate(a[i], bMinusC);
-            NumberExpression aTimesBMinusCPlusD = addition.evaluate(aTimesBMinusC, d[i]);
-            result[i] = modulo.evaluate(aTimesBMinusCPlusD, a[i]);
-        }
-
-        return result;
-    }
-
-    /**
      * Filters the given array of number expressions based on the given predicate.
-     * The returned array should contain only the number expressions that satisfy the predicate in the same order as they appear in the input array.
+     * The returned array should contain only the number expressions that satisfy
+     * the predicate in the same order as they appear in the input array.
      * This means there should be no null values in the returned array.
      *
-     * @param numbers the array of number expressions to filter
+     * @param numbers   the array of number expressions to filter
      * @param predicate the predicate to filter the number expressions
      * @return An array of number expressions that satisfy the predicate.
      */
